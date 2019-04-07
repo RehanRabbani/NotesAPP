261a06009f8fe561b5c817a4c97ee8002fe6835e
RehanRabbani <rabbanirehan6@gmail.com>
2019-04-07 23:16:26 +0500

    add functionality of local storage

--- script.js
+++ script.js
@@ -1,11 +1,50 @@
-function one(sp){
-    let getId = document.querySelector('#para');
-    if(getId.contentEditable==='true'){
-        getId.contentEditable = 'false';
-        sp.innerHTML = `<i class="fab fa-500px"></i>`;
+let Content;
+function one(sp) {
+    let getId = document.querySelector('#para') ;
+     Content = document.querySelector('#para').textContent;
+    localStorage.setItem('note',Content);
+ getContentFromLocal = localStorage.getItem('note');
+    Content.textL=getContentFromLocal;
+   
+    console.log(Content);
+    console.log(getContentFromLocal);
+    // let getId = document.querySelector("#para");
+    if (getId.contentEditable  === "false") {
+      getId.contentEditable  = "true";
+     
+      sp.innerHTML = `<i class="fab fa-500px"></i>`;
+     
+    } else {
+      getId.contentEditable  = "false";
+     
+      sp.innerHTML = `<i class="fas fa-check"></i>`;
+      
     }
-    else{
-        getId.contentEditable = 'true';
-        sp.innerHTML = `<i class="fas fa-check"></i>`;
-    }
+  }
+function del(){
+    let parentElement=document.querySelector('body');
+    let nodeToRemove = parentElement.childNodes[3];
+    parentElement.removeChild(nodeToRemove);
+    
+}
+function add()
+{
+    let parentElement = document.querySelector('body');
+    parentElement.innerHTML+=`<div class="parent">
+    <header>
+    
+        <span onclick="one(this)">
+            <i class="fas fa-check"></i>
+        </span>
+        <span onclick="del ()">
+            <i class="far fa-trash-alt"></i>
+        </span>
+    </header>
+
+    <section>
+        <p id="para" contentEditable="true" >jkdsjf</p>
+    </section>
+
+</div>
+`
 }
